c242c9dda311cf2466bb23a8fb07586519777466
tendetta19 <33647263+tendetta19@users.noreply.github.com>
2024-11-06 07:15:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // List of predefined images to be used in the puzzle
 const images = [ 
+	'https://pbs.twimg.com/media/GbcYaxjb0AAC_ry?format=jpg&name=4096x4096',
 	'20241105_123708.jpg',
 	'https://pbs.twimg.com/media/GbcdUGwbsAAUZcF?format=jpg&name=large',
 	'https://pbs.twimg.com/media/GbYBUdHa8AAYBVn?format=jpg&name=4096x4096',
